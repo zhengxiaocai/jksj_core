ae149aaaee8b2f3fc1bfbd3dda88fa67948f8802
zhengxiaocai <zhengyuan_tester@163.com>
2020-07-20 23:01:25 +0800

    字典操作

--- ch04/py0401_dict_set.py
+++ ch04/py0401_dict_set.py
@@ -2,96 +2,71 @@
 从py3.7开始，dict为有序的了。
 
 相比于列表 元组，字典性能更优。
+特别是查找 添加 删除操作时，常数时间复杂度内完成。
+
+集合和字典基本相同，唯一的区别，集合没有键值配对，是一系列无序的、唯一的元素组合。
 
 测试执行速度：timeit.timeit('python script')
 """
-import timeit
-
-
-def find_product_price(products, product_id):
-    for product in products:
-        if product[0] == product_id:
-            return product[1]
-    return None
-
 
 if __name__ == '__main__':
-    # 声明字典的几种形式
-    d1 = {'name': 'jason', 'age': 20, 'gender': 'male'}
-    d2 = dict([('name', 'jason'), ('age', 20), ('gender', 'male')])
-    d3 = dict(name='jason', age=20, gender='male')
-    print(d1 == d2 == d3)
+    # 字典的创建方式
+    dict01 = {'name': 'jason', 'age': 20, 'gender': 'male'}
+    dict02 = dict({'name': 'jason', 'age': 20, 'gender': 'male'})
+    dict03 = dict([('name', 'jason'), ('age', 20), ('gender', 'male')])
+    dict04 = dict(name='jason', age=20, gender='male')
+    print(dict01 == dict02 == dict03 == dict04)
 
-    # 声明集合的两种方式
-    s1 = {1, 2, 3}
-    s2 = set([1, 2, 3])
-    print(s1 == s2)
+    # 集合的创建方式
+    set01 = {1, 2, 3}
+    set02 = set([1, 2, 3])
+    print(set01 == set02)
 
-    print(timeit.timeit('s1 = {1, 2, 3}'))
-    print(timeit.timeit('s2 = set([1, 2, 3])'))
+    # 字典和集合，无论键或者值，都是混合类型
+    set03 = {1, 'hello', 5.0}
 
-    # 根据键访问，如果键不存在，就会报异常KeyError
-    print(d3['name'])
-    try:
-        print(d3['abc'])
-    except KeyError as e:
-        print('Key Error.')
+    # 元素访问，可以直接访问键；如果不存在，就会抛出异常
+    dict05 = {'name': 'jason', 'age': 20}
+    print(dict05['name'])
+    # print(dict05['location']) KeyError: 'location'
 
-    # 可以用字典的get方法，指定找不到的时候的默认值。默认是None
-    print(d3.get('age'))
-    print(d3.get('abc'))
-    print(d3.get('abc', 'abc not found'))
+    # 相比于这种粗暴的方式，用get会更温和一些，因为，可以提供一个默认值
+    # 如果不提供默认值，获取不到的时候，默认返回None
+    print(dict05.get('name'))
+    print(dict05.get('location'))
+    print(dict05.get('location', 'None'))
 
-    # 集合不支持索引操作，集合是一个哈希表。TypeError
-    try:
-        print(s1[0])
-    except TypeError as e:
-        print('集合不支持索引操作。', e)
+    # 集合不支持索引操作，因为集合本质上是个哈希表
+    # print(set03[0])   TypeError: 'set' object is not subscriptable
 
-    # 判断是否是字典/集合的一项 item in dict/set
-    print('name' in d1)
-    print('names' in d1)
-    print(1 in s1)
+    # 要判断一个元素在不在字典 集合内，可以用in
+    dict06 = {'name': 'jason', 'age': 20}
+    set04 = {1, 2, 3}
+    print('name' in dict06)
+    print('location' in dict06)
+    print(1 in set04)
+    print(4 in set04)
 
-    # 增加 dict[new_key] = value
-    d1['dob'] = '1999-02-01'
-    print(d1)
+    # 增加 删除 更新
+    dict06['gender'] = 'male'
+    dict06['dob'] = '1992-05-13'
+    print(dict06)
+    dict06['dob'] = '1993-05-13'
+    print(dict06)
+    dict06.pop('dob')
+    print(dict06)
 
-    # 删除 dict.pop(key)；pop(不存在的key) 报错KeyError
-    d1.pop('name')
-    print(d1)
-    try:
-        d1.pop('abc')
-    except KeyError as e:
-        print('KeyError')
-
-    # 集合增加 set.add(value)
-    s1.add(4)
-    print(s1)
-
-    # 集合删除 set.remove(value)
-    s1.remove(1)
-    print(s1)
+    set04.add(4)
+    print(set04)
+    set04.remove(1)
+    print(set04)
 
     # 排序
-    d4 = {'b': 1, 'a': 2, 'c': 10}
-    d4_sorted_by_key = sorted(d4.items(), key=lambda x: x[0])
-    d4_sorted_by_value = sorted(d4.items(), key=lambda x: x[1])
-    print(d4_sorted_by_key)
-    print(d4_sorted_by_value)
+    dict07 = {'b': 1, 'a': 2, 'c': 10}
+    dict07_sorted_by_key = sorted(dict07.items(), key=lambda x: x[0])
+    print(dict07_sorted_by_key)
+    dict07_sorted_by_value = sorted(dict07.items(), key=lambda x: x[1])
+    print(dict07_sorted_by_value)
 
-    s3 = {3, 4, 2, 1}
-    print(sorted(s3))
-
-    # 实际应用，如果存在list里
-    products_list = [(143121312, 100), (432314553, 30), (32421912367, 150), (937153201, 30)]
-    print(f'The price of product 432314553 is {find_product_price(products_list, 432314553)}')
-
-    # 实际应用，在字典中
-    products_dict = {143121312: 100, 432314553: 30, 32421912367: 150}
-    print(f'The price of product 432314553 is {products_dict[432314553]}')
-
-
-
-
-
+    set05 = {3, 4, 2, 1}
+    print(sorted(set05))
